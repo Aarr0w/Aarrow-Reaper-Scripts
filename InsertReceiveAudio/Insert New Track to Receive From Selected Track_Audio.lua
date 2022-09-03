7e0f1080b644015cad0120966b53c200ac228564
Aaron Walker <83385885+Aarr0w@users.noreply.github.com>
2022-09-03 16:52:32 -0500

    Update Insert New Track to Receive From Selected Track_Audio.lua

--- InsertReceiveAudio/Insert New Track to Receive From Selected Track_Audio.lua
+++ InsertReceiveAudio/Insert New Track to Receive From Selected Track_Audio.lua
@@ -2,9 +2,9 @@
 Description: Creates a new track after the selected track which receives the selected track's audio
 Version: 1.0
 Author: Aarrow 
-Donation: I'll add a paypal later...
+Donation: 
           
-Links: Aarrow's Website [ to be added later ...] 
+Links:
 
 About:
   Creates a new track after the selected track which receives the selected track's audio
