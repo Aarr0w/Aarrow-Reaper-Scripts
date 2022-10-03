965d26b6c0a2aee1820ec575e5da6550195e5a46
Aaron Walker <aa.walk22@gmail.com>
2022-10-02 22:42:56 -0500

    cleaning

--- RandomizeStretchMarkers/randomize selected take stretch marker slopes and positions.lua
+++ RandomizeStretchMarkers/randomize selected take stretch marker slopes and positions.lua
@@ -1,40 +1,41 @@
 --[[ 
 Description: Randomizes slope and position of stretch markers
-Version: 1.0
+Version: 1.1
 Author: Aarrow 
 Donation: https://paypal.me/Aarr0w
   
           
 Links: https://linktr.ee/aarr0w
- 
+  
 About:
   Glitch it up, yo
 --]]
-
+   
 local item = reaper.GetSelectedMediaItem(0,0)
 local take = reaper.GetTake(item,0)
 local position 
 local slope = 1  
 
 math.randomseed(os.time())
-
+   
 -----------------------------------------------------------------------------
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 -----------------------------------------------------------------------------
-
+ 
 for i = 0, reaper.GetTakeNumStretchMarkers(take) do
   
   retval, position, srcpos = reaper.GetTakeStretchMarker(take, i)
   slope = reaper.GetTakeStretchMarkerSlope(take, i)
   
  
-  position = position + math.random(-100,100)*0.01 -- not exactly sure how this works but.... it does :)
+  position = position + math.random(-50,50)*0.01 -- not exactly sure how this works but.... it does :)
   
   slope = math.random(-100,100)* 0.01
   reaper.SetTakeStretchMarkerSlope(take,i,slope)
  
   
+  --reaper.SetTakeStretchMarker(take,i,position, position - math.random(100)*0.01)  
   reaper.SetTakeStretchMarker(take,i,position)  
   --retval, position, srcpos = reaper.GetTakeStretchMarker(take, i)
 
